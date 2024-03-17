cda051cb362d954029f9d0642b379ceb0164d1e8
FerF527 <fernandofarias527@gmail.com>
2024-03-17 20:20:59 -0300

    ejercicio 12 pasar el ejercicio 2 d) a python

---
+++ practica1/ejercicio12_2_d.py
@@ -0,0 +1,13 @@
+# Pasar los ejercicios 2, 3, 4 y 5 hechos en papel a programas en Python y probarlos.
+
+# Ejercicio 2
+# Dar expresiones de código tal cual como las escribiría en python para calcular las siguientes expresiones matemáticas.
+# d) ((x+y)**2)**2
+
+print("Ejercicio d: ");
+valor_x = input("Ingrese el valor x: ");
+valor_y = input("Ingrese el valor y: ");
+x = float(valor_x);
+y = float(valor_y);
+resultado = ((x+y)**2)**2;
+print("El resultado es: ",resultado);
