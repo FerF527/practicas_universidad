2100fcf4be5935f3feb873d3f2602dd6addd6689
FerF527 <fernandofarias527@gmail.com>
2024-03-17 00:27:59 -0300

    ejercicio 7

---
+++ practica1/ejercicio7.py
@@ -0,0 +1,18 @@
+# Escribir un programa en la computadora que imprima en pantalla
+# v
+# e
+# r
+# t
+# i
+# c
+# a
+# l
+
+print("v \n");
+print("e \n");
+print("r \n");
+print("t \n");
+print("i \n");
+print("c \n");
+print("a \n");
+print("l \n");
