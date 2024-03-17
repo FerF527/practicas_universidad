318f09db6c0276b345cedf6613ae9440acf7ad8a
FerF527 <fernandofarias527@gmail.com>
2024-03-17 00:20:28 -0300

    ejercicio 5

--- practica1/ejercicio4.py
+++ practica1/ejercicio4.py
@@ -1,5 +1,5 @@
-# Escribir en papel un programa que pida al usuario un valor entero y devuelva el siguiente número
-# más grande.
+# Escribir un programa que pida al usuario un valor entero y devuelva el siguiente número más grande.
+
 numero_ingresado = input("Ingrese un número entero ");
 numero_ingresado = int(numero_ingresado);
 numero_siguiente = numero_ingresado + 1
---
+++ practica1/ejercicio5.py
@@ -0,0 +1,9 @@
+# Escribir un programa que pida al usuario dos números y muestre por pantalla su promedio.
+
+primer_numero = input("Ingrese el primer número: ");
+segundo_numero = input("Ingrese el segundo número: ");
+primer_numero = float(primer_numero);
+segundo_numero = float(segundo_numero);
+promedio = (primer_numero + segundo_numero) / 2;
+print("El promedio de los dos números es: ");
+print(promedio);
