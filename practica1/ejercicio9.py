a718aa3149ec0e2c12ec30c20fd64e91e6d37a04
FerF527 <fernandofarias527@gmail.com>
2024-03-17 00:38:47 -0300

    ejercicio 9

---
+++ practica1/ejercicio9.py
@@ -0,0 +1,7 @@
+# Escribir un programa en Python que imprima la siguiente figura en la pantalla utilizando un solo print:
+# *
+# ***
+# *****
+# *******
+# *********
+print("*\n***\n*****\n*******\n*********");
