3580501f235358118aed5bffc1b549d03fd32672
FerF527 <fernandofarias527@gmail.com>
2024-03-17 00:06:19 -0300

    ejercicio 3 y 4

---
+++ practica1/ejercicio3.py
@@ -0,0 +1,9 @@
+# Escribir un programa que pida al usuario dos valores cualquiera e imprima en pantalla la suma de ambos.
+
+a = input("Ingrese el primer número a sumar ");
+a = int(a);
+b = input("Ingrese el segundo número a sumar ");
+b = int(b);
+c = a + b;
+print("El total es: ");
+print(c);
---
+++ practica1/ejercicio4.py
@@ -0,0 +1,6 @@
+# Escribir en papel un programa que pida al usuario un valor entero y devuelva el siguiente número
+# más grande.
+numero_ingresado = input("Ingrese un número entero ");
+numero_ingresado = int(numero_ingresado);
+numero_siguiente = numero_ingresado + 1
+print(numero_siguiente);
