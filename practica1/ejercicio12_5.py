48a42029e6230eeafa2bb664c76694f7fc3fe39f
FerF527 <fernandofarias527@gmail.com>
2024-03-24 14:03:08 -0300

    ejercicio 12 pasar el ejercicio 5 a python

---
+++ practica1/ejercicio12_5.py
@@ -0,0 +1,13 @@
+# Pasar los ejercicios 2, 3, 4 y 5 hechos en papel a programas en Python y probarlos.
+
+# Ejercicio 5
+
+# Escribir en papel un programa que pida al usuario dos números y muestre por pantalla su promedio.
+
+primer_numero = input("Ingrese el primer número: ");
+segundo_numero = input("Ingrese el segundo número: ");
+primer_numero = float(primer_numero);
+segundo_numero = float(segundo_numero);
+promedio = (primer_numero + segundo_numero) / 2;
+print("El promedio de los dos números es: ");
+print(promedio);
