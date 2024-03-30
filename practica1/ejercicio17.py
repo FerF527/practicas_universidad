8b5a83019e29ab5ac59d3eb168562b08cec88d5c
FerF527 <fernandofarias527@gmail.com>
2024-03-30 15:22:21 -0300

    ejercicio 19

--- practica1/ejercicio17.py
+++ practica1/ejercicio17.py
@@ -1,3 +1,8 @@
+# La empresa que administra los cajeros automáticos lo contrata para que programen la entrega de
+# billetes, el usuario ingresa la cantidad de dinero que desea y usted debe indicar cuantos billetes de cada
+# denominación se debe entregar. Es importante entregar siempre la menor cantidad de billetes. Ayuda:
+# El operador % da el resto de la división a/b, y el operador // da la parte entera del cociente de a/b.
+
 dinero = int(input("Ingrese el monto a retirar "));
 aux = dinero;
 billetes_de_1000 = dinero // 1000;
--- practica1/ejercicio18.py
+++ practica1/ejercicio18.py
@@ -1,3 +1,7 @@
+# Escribir un programa en Python que pida al usuario una cantidad de segundos y muestre en pantalla
+# la cantidad de días, minutos y segundos que representa. Por ejemplo, si el usuario ingresa 86461 segundos
+# el programa debe mostrar por pantalla: 1 día 0 horas 1 minuto 1 segundo.
+
 segundos = int(input("Ingrese los segundos "));
 aux = segundos;
 dias = segundos // 86400;
