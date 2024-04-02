8c2f90e3b46a1e39a8a29a6ff04ce89dc0fa808f
FerF527 <fernandofarias527@gmail.com>
2024-04-02 15:28:51 -0300

    ejercicio 6 a

---
+++ practica2/ejercicio6a.py
@@ -0,0 +1,10 @@
+# a) Escribir en papel un programa que pida al usuario dos números, y que muestre en
+# pantalla al mayor de ambos. Luego hacer 3 corridas de escritorio, luego pasarlo a
+# Python y por último correr el programa con los valores iniciales de las corridas y
+# verificar que funciona como se esperaba.
+numero1 = int(input("Ingrese el primer número "));
+numero2 = int(input("Ingrese el segundo número "));
+if numero1 > numero2 :
+    print("El primer número es el mayor:",numero1);
+else :
+    print("El segundo número es el mayor:",numero2);
