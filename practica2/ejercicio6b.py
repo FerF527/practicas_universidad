0e5cdc9273dae9b8acb290f476d45c2b40276aee
FerF527 <fernandofarias527@gmail.com>
2024-04-02 15:43:12 -0300

    ejercicio 6 b

---
+++ practica2/ejercicio6b.py
@@ -0,0 +1,11 @@
+# a) Escribir en papel un programa que pida al usuario dos números, y que muestre en
+# pantalla al mayor de ambos. Luego hacer 3 corridas de escritorio, luego pasarlo a
+# Python y por último correr el programa con los valores iniciales de las corridas y
+# verificar que funciona como se esperaba.
+# b) Ídem anterior pero para encontrar el menor
+numero1 = int(input("Ingrese el primer número "));
+numero2 = int(input("Ingrese el segundo número "));
+if numero1 < numero2 :
+    print("El primer número es el menor:",numero1);
+else :
+    print("El segundo número es el menor:",numero2);
