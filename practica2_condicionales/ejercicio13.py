9ee3cecdf8ca54a97f76a8ee0aae1ab68ccaf0d5
FerF527 <fernandofarias527@gmail.com>
2024-04-04 23:24:29 -0300

    ejercicio 13 condicionales

---
+++ practica2_condicionales/ejercicio13.py
@@ -0,0 +1,9 @@
+# Escribir un programa que pida al usuario dos enteros y que luego muestre si el primero es
+# mayor que el segundo o viceversa.
+primer_numero = int(input("Ingrese el primer número entero "));
+segundo_numero = int(input("Ingrese el segundo número entero "));
+
+if primer_numero > segundo_numero :
+    print("El primer número es mayor",primer_numero);
+else :
+    print("El segundo número es mayor",segundo_numero);
