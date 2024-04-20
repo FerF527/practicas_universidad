02c60aa0770353c5d8dcdc9b16287ff928428110
FerF527 <fernandofarias527@gmail.com>
2024-04-20 15:19:42 -0300

    ejercicios 5 y 6 ciclos con for in

---
+++ practica3_ciclos/ejercicio5_for.py
@@ -0,0 +1,5 @@
+# a) Hacer un programa que muestre, mediante un ciclo, los números desde el 8 hasta el
+# 3 (8, 7, 6, 5, 4, 3).
+print("Mostrando números del 8 al 3");
+for numero in range(8,2,-1):
+    print(numero);
---
+++ practica3_ciclos/ejercicio6_for.py
@@ -0,0 +1,5 @@
+# a) Hacer un programa que muestre, mediante un ciclo, los números desde el 15 hasta
+# el 6 pero salteando de a tres (15, 12, 9, 6).
+print("Mostrando números del 15 al 6 en saltos de 3");
+for numero in range(15,5,-3):
+    print(numero);
