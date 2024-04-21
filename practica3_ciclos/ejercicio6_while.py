82ce65d5770d06eb2c6ff0c4fa838fd720ff965b
FerF527 <fernandofarias527@gmail.com>
2024-04-20 23:33:31 -0300

    ejercicio 6, ciclos con while

---
+++ practica3_ciclos/ejercicio6_while.py
@@ -0,0 +1,7 @@
+# a) Hacer un programa que muestre, mediante un ciclo, los números desde el 15 hasta
+# el 6 pero salteando de a tres (15, 12, 9, 6).
+print("Mostrando números del 15 al 6 en saltos de 3");
+numero_inicio = 15;
+while numero_inicio > 5 :
+    print(numero_inicio);
+    numero_inicio = numero_inicio - 3;
