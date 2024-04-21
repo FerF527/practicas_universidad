b85441dcebfc409886dd2252eafd1249d602b9ac
FerF527 <fernandofarias527@gmail.com>
2024-04-20 23:28:51 -0300

    ejercicio 5 ciclos con while

---
+++ practica3_ciclos/ejercicio5_while.py
@@ -0,0 +1,7 @@
+# a) Hacer un programa que muestre, mediante un ciclo, los números desde el 8 hasta el
+# 3 (8, 7, 6, 5, 4, 3).
+print("Mostrando números del 8 al 3");
+numero_inicio = 8;
+while numero_inicio >= 3 :
+    print(numero_inicio);
+    numero_inicio = numero_inicio -1;
