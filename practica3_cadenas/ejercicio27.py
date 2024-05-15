ecb8e7b24973aaa4a26bfaa2313d6059898079dd
FerF527 <fernandofarias527@gmail.com>
2024-05-15 20:33:22 -0300

    ejercicio 27 cadenas

---
+++ practica3_cadenas/ejercicio27.py
@@ -0,0 +1,15 @@
+# Hacer una programa que, dada una palabra, la escriba recuadrada por asteriscos. Por ejemplo,
+# si la palabra es "Ganaste", el programa debería escribir:
+# ***********
+# * Ganaste *
+# ***********
+
+palabra = input("Ingrese un texto ")
+largo = len(palabra)
+
+#espacios iniciales 
+for i in range(0,largo + 4) :
+    print("*",end="")
+print("\n* " + palabra + " *")
+for i in range(0,largo + 4) :
+    print("*",end="")
