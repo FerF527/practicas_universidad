7f8ea7992fe99ba2eb54765bef23e36ba4a1f948
FerF527 <fernandofarias527@gmail.com>
2024-05-16 21:13:54 -0300

    ejercicio 3 funciones

---
+++ pratica4_funciones/ejercicio3a_b.py
@@ -0,0 +1,6 @@
+# a) Escribir una función que reciba como parámetro una cadena y la muestre en pantalla
+# 3 veces.
+# b) Guardar esta definición de función en un archivo.
+def mostrar_cadena(cadena) :
+    for i in range (0,3) :
+        print(cadena)
---
+++ pratica4_funciones/ejercicio3c.py
@@ -0,0 +1,5 @@
+# c) Hacer un programa que le pida al usuario una cadena y que la muestre en pantalla
+# 3 veces utilizando la función definida anteriormente.
+from ejercicio3a_b import *
+texto = input("Ingrese un texto ")
+mostrar_cadena(texto)
