35e0634f15b5aed9790cc9e61bd9702dee393bab
FerF527 <fernandofarias527@gmail.com>
2024-05-17 23:35:42 -0300

    ejercicio 7

---
+++ pratica4_funciones/ejercicio7a.py
@@ -0,0 +1,5 @@
+# a) Escribir una función que se llame elevarAlCubo que tome un número y retorne el
+# valor de ese número al cubo.
+def elevarAlCubo(num) :
+    resultado = num**3
+    return resultado
---
+++ pratica4_funciones/funcionCubo.py
@@ -0,0 +1,4 @@
+# b) Guardar el ejercicio anterior en un archivo llamado funcionCubo.py
+def elevarAlCubo(num) :
+    resultado = num**3
+    return resultado
