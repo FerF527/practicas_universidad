5a53f83867af10353c06279072a440e56c867bb9
FerF527 <fernandofarias527@gmail.com>
2024-05-17 23:06:23 -0300

    ejercicio 5 funciones

---
+++ pratica4_funciones/ejercicio5.py
@@ -0,0 +1,8 @@
+# Definir una función que devuelva el valor absoluto de un número. (Hacerlo sin utilizar la
+# función abs)
+def valor_absoluto(num) : 
+    if num < 0 :
+        valor_absoluto = num * - 1
+    else :
+        return num
+    return valor_absoluto
