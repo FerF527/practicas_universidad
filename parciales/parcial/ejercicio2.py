8c6c18e59f5459f01b4f764a035cf2860080dbf2
GermanPerezz <gergperez0021@gmail.com>
2024-05-30 21:28:09 -0300

    ejercicio 2 del parcial comentado.

--- parciales/parcial/ejercicio2.py
+++ parciales/parcial/ejercicio2.py
@@ -1,3 +1,11 @@
+# Ejercicio 2
+# Escribir una función paresEnLaInterseccion que reciba como parámetro dos listas de números y devuelva la cantidad de números pares en la intersección de ambas listas.
+# Por ejemplo:
+# Si una lista fuera: [24, 125, 32, 10, 56, 100, 90, 17]
+# y la otra lista fuera: [199, 254, 17, 24, 32, 5, 12, 56, 145]
+# la intersección de ambas sería: [24, 17, 32, 56]
+# Por ende, la función paresEnLaInterseccion debe devolver 3, ya que hay 3 números pares en la intersección.
+
 def paresEnLaInterseccion(lista1, lista2):
     interseccion = []
 
