d08914ed9b5f444255a8215c52a220483696b591
GerSchroeder37 <gerimport37@gmail.com>
2024-05-31 23:08:06 -0300

    subo ejercicio 6 de la practica 5

--- practica5_listas/ejercicio6.py
+++ practica5_listas/ejercicio6.py
@@ -1,2 +1,15 @@
 # Definir una funcion que tome una lista y devuelva True si tiene al menos un elemento repetido,
 # y False en caso contrario
+def chequeoRepetido(lista):
+    newlist=[]
+    for elemento in lista:
+        if elemento not in newlist:
+            newlist.append(elemento)
+        else:
+            return False
+    return True
+
+
+#programa principal
+list=["a","b","c","a"]
+print(chequeoRepetido(list))
