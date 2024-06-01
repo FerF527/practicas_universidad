5ac5fb9ae8526b655b656cbd551b27a2b4d670f8
FerF527 <fernandofarias527@gmail.com>
2024-05-31 21:15:58 -0300

    test de ejercicio 7 practica de listas

--- practica5_listas/ejercicio7.py
+++ practica5_listas/ejercicio7.py
@@ -1,10 +1,8 @@
+# Definir una función llamada dondeAparece que tome una lista de enteros y un entero llamado
+# blanco como parámetros, y devuelva el primer índice donde blanco aparece en el arreglo, si lo
+# hace, y -1 en caso contrario.
 def dondeAparece (lista,blanco):
     for i in range (len(lista)):
         if lista [i] == blanco:
             return i
     return -1
-
-lista=[10,20,30,40,50,60,70,80]
-entero=int(input("ingrese un entero"))
-indice=dondeAparece(lista,entero)
-print(indice)
---
+++ practica5_listas/ejercicio7test.py
@@ -0,0 +1,5 @@
+from ejercicio7 import *
+lista=[10,20,30,40,50,60,70,80]
+entero=int(input("ingrese un entero"))
+indice=dondeAparece(lista,entero)
+print(indice)
