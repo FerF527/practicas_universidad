fa66f9c3e81666c5813e8c1982779dc65a0c5409
FerF527 <fernandofarias527@gmail.com>
2024-06-02 14:36:30 -0300

    programa principal ejercicio 10

--- practica5_listas/ejercicio10.py
+++ practica5_listas/ejercicio10.py
@@ -1,12 +1,10 @@
+# Escribir una función llamada maximoIndice que tome una lista de números y devuelva el
+# índice del máximo elemento.
 def maximoindice(lista):
     if not lista:
         return None
-    indice_mas_alto = 0
+    indice_mas_alto = -1
     for i in range(len(lista)):
         if lista[i] > lista[indice_mas_alto]:
             indice_mas_alto = i
     return indice_mas_alto
-
-lista = [13, 24, 65, 78, 6]
-indice_mas_alto = maximoindice(lista)
-print(f"El índice del número más alto en la lista es: {indice_mas_alto}")
---
+++ practica5_listas/ejercicio10pp.py
@@ -0,0 +1,5 @@
+##programa principal ejercicio 10
+from ejercicio10 import maximoindice
+lista = [13, 24, 65, 6, 78]
+indice_mas_alto = maximoindice(lista)
+print(f"El índice del número más alto en la lista es: {indice_mas_alto}")
