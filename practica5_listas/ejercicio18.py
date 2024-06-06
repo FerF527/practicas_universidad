84dff9ff62098ff75a3789f9a02da2b858941279
GermanPerezz <gergperez0021@gmail.com>
2024-06-05 23:36:13 -0300

    exercise

--- practica5_listas/ejercicio18.py
+++ practica5_listas/ejercicio18.py
@@ -1,4 +1,4 @@
-# Define a function que tome un integer n y return los primeros n primos.
+# Define a function that takes an integer n and return the first cousins numbers "n"
 def is_cousin(n):
     for i in range(2,n):
         if n%i==0:
@@ -6,13 +6,13 @@ def is_cousin(n):
     return True
         
 def return_cousins(n):
-    primos=[1]
+    cousins=[1]
     for i in range(2,n):
         if is_cousin(i):
-            primos.append(i)
-    return primos 
+            cousins.append(i)
+    return cousins 
     
 
 n=int(input("input a number"))
-program=return_cousins(n)
-print("los primeros numbers primos de:",n," son:",program) 
+cousin_list=return_cousins(n)
+print("the first cousins numbers of:",n," are:",cousin_list) 
